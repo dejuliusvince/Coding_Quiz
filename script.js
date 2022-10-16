c5943f8d6f70c2dec45ae84d685ac33e5ec8ab3b
vincedejulius <dejuliusvince@gmail.com>
2022-10-16 17:33:21 -0400

    logic to display quiz, begin logic for selecting responses

--- script.js
+++ script.js
@@ -6,15 +6,48 @@
 -Time the quiz
 */
 
-var questions = [
-  question:'',
-  responses: []
-]
+displayedQuestion = 0;
+
+let questions = [
+  {
+    question: 'Which of the following keywords can define a variable in Javascript?',
+    responses: ['var', 'let', 'both'],
+    answer: 'all of the above'
+  },
+  {
+    question: 'Which of the following functions will serialize an object into a JSON string?',
+    responses: ['parse()', 'convert()', 'stringify()'],
+    answer: 'stringify()',
+
+  },
+];
+
+const showQuestion = () => {
+  document.getElementById("question").innerHTML= `
+  <p>
+  ${questions[displayedQuestion].question}
+  </p>
+  `
+
+  document.getElementById("response").innerHTML= `
+  <p>
+  ${questions[displayedQuestion].responses[0]}
+  </p>
+  <p>
+  ${questions[displayedQuestion].responses[1]}
+  </p>
+  <p>
+  ${questions[displayedQuestion].responses[2]}
+  </p>
+  `
+}
 
 
 
 
 
-document.getElementById("start-button").addEventListener('click', event => {
-  document.getElementById("quiz").classList.remove("hide-quiz")
-})
+
+  document.getElementById("start-button").addEventListener('click', event => {
+    document.getElementById("quiz").classList.remove("hide-quiz")
+    showQuestion()
+  })
