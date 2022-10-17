4eac5c0fa1390e7de7c5064281db659b8ea683ce
vincedejulius <dejuliusvince@gmail.com>
2022-10-17 11:45:49 -0400

    descriptive comments for functionality and drafted readme file

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# My First Coding Quiz
+# My First Coding Quiz [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT) 
+
+## Description
+  This project is a simple coding quiz that asks the user five simple coding questions, tracks their score as they answer the questions, and has a time limit of 60 seconds. If a wrong answer is selected, five extra seconds will be subtracted from the timer. When the timer reaches zero, a game over message is shown to the user and they are given the option to refresh the page and begin the quiz again. Further development on this project will include saving user data into locale storage, such as their score and their initials.
+
+## Screenshot
+
+
+
+## Installation 
+  This application can be downloaded from my GitHub profile.
+
+
+## Usage
+  This can be used for a quick simple coding practice test on fundamental Javascript concepts.
+
+
+## Contributing
+  The user is free to use this application as they choose.
+
+
+## Tests
+  This app can be launched within your browser for use and testing.
+
+
+  ## License
+  This repo is licensed under [MIT License](https://opensource.org/licenses/MIT).
+
+  ## Questions
+  Email address: dejuliusvince@gmail.com <br />
+  GitHub: dejuliusvince
+
+  ## Links
+  link to deployed app: 
+  link to GitHub repository:
+
+  
--- script.js
+++ script.js
@@ -5,15 +5,14 @@
 -Allow user to select their response
 -Time the quiz
 */
+
+//variables for the starting points of the user score, the countdown timer, and the first question in the array of questions
 let score = 0
 let timer = 60
 
-
-
-
 displayedQuestion = 0
 
-
+//Array of questions for the quiz
 let questions = [
   {
     question: 'Which of the following keywords can define a variable in Javascript?',
@@ -42,14 +41,16 @@ let questions = [
   }
 ]
 
+//Function to iterate through questions as the user selects answers
 const showQuestion = () => {
+  // checks to see if the user has finished the quiz, if not finished will move to next question
   if (displayedQuestion == questions.length) {
     document.getElementById("quiz").innerHTML = `
     <h1>You have completed the quiz!</h1>
     <h3>Score: ${score}/25</h3> 
   `
     document.getElementById("timer").classList.add("hide-form")
-   
+   //Shows the form to save user initials and score after quiz is finished
     document.getElementById("enterInitials").classList.remove("hide-form")
   } else {
     document.getElementById("questionDiv").innerHTML = `
@@ -57,7 +58,7 @@ const showQuestion = () => {
   ${questions[displayedQuestion].question}
   </p>
   `
-
+//responses need datasets to compare response vs answer and check if they have been answered correctly
     document.getElementById("responseDiv").innerHTML = `
   <p
   class="response"
@@ -83,27 +84,8 @@ const showQuestion = () => {
   `}
 }
 
-
-
-document.addEventListener('click', event => {
-  if (event.target.classList.contains('response')) {
-    if (event.target.dataset.response == event.target.dataset.answer) {
-      displayedQuestion += 1
-      score += 5
-      showQuestion()
-    } else {
-      const subtractTime = () => { timer -= 5 }
-      displayedQuestion += 1
-      subtractTime()
-      showQuestion()
-    }
-  }
-})
-
-const endQuiz = () => {
-
-}
-
+//Function to be called when the user starts the quiz, counting down from 60 
+//and ending the quiz if timer hits 0  
 const countdownStart = () => {
   if (timer <= 0) {
     timeAtZero()
@@ -117,7 +99,8 @@ const countdownStart = () => {
   }
 }
 
-
+// Used by countdownStart function to end the Quiz and show current score
+// if time runs out 
 const timeAtZero = () => {
   clearInterval()
   document.getElementById("quiz").innerHTML = `
@@ -132,16 +115,37 @@ const timeAtZero = () => {
 
 }
 
+//If the user clicks a response to a question, will check to see if they
+//answered correctly and proceed accordingly.
+document.addEventListener('click', event => {
+  if (event.target.classList.contains('response')) {
+    if (event.target.dataset.response == event.target.dataset.answer) {
+      displayedQuestion += 1
+      score += 5
+      showQuestion()
+    } else {
+      //If user answers a question wrong, an extra 5 seconds will be deducted from the timer
+      const subtractTime = () => { timer -= 5 }
+      displayedQuestion += 1
+      subtractTime()
+      showQuestion()
+    }
+  }
+})
+
+//Function attempting to save user initials and score in local storage. 
+//(Could not get data to save properly, future development will address this issue)
 document.getElementById("saveButton").addEventListener('submit', event => {
   event.preventDefault();
   let newSavedData = {
     score: score,
     initials: document.getElementById("userInitials").value
   }
-  console.log(newSavedData)
+  localStorage.setItem(newSavedData)
 })
 
 
+//Start button functionality to start quiz and begin countdown 
 document.getElementById("start-button").addEventListener('click', event => {
   document.getElementById("quiz").classList.remove("hide-quiz")
   setInterval(countdownStart, 1000)
